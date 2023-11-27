9d869a6a8580ab4336b1986fac2612521c516e98
gianlucanied <gianluca.nieddu96@gmail.com>
2023-11-27 15:28:22 +0100

    EX Pari o Dispari

---
+++ Pari e Dispari/script.js
@@ -0,0 +1,53 @@
+/* Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto. */
+
+// Chiediamo a User se sceglie Pari o Dispari
+
+const pariODispari = prompt('Scegli se Pari o Dispari');
+
+// Stampiamo la sua scelta 
+
+console.log (`Hai scelto "${pariODispari}"`);
+
+// Chiediamo a User un numero da 1 a 5
+
+const numberUser = parseInt(prompt('Inserisci un numero da 1 a 5'));
+
+// Funzione per verificare se un numero è pari
+
+function pari(number) {
+    return number % 2 === 0;
+  }
+
+// Stampiamo il numero scelto 
+
+console.log (`Hai scelto il numero "${numberUser}"`);
+
+// Generiamo un numero casuale da 1 a 5 per il computer con una funzione
+
+function generaRandomNumber() {
+    return Math.floor(Math.random() * 5) + 1;
+  }
+
+const computerNumber = generaRandomNumber();
+// Stampiamo il numero casuale del PC
+
+console.log (`Il numero casuale del PC è: "${computerNumber}"`)
+
+// Sommiamo i due numeri e stabiliamo se questa è pari o dispari (con una funzione)
+
+const sommaNumber = numberUser + computerNumber;
+
+const isSumEven = pari(sommaNumber);
+
+// Dichiariamo il vincitore
+
+if ((pariODispari === "pari" && isSumEven) || (pariODispari === "dispari" && !isSumEven)) {
+
+    console.log(`Hai vinto! La somma (${numberUser} + ${computerNumber} = ${sommaNumber}) è ${pariODispari}.`);
+  } else {
+    
+    console.log(`Hai perso! La somma (${numberUser} + ${computerNumber} = ${sommaNumber}) è ${(isSumEven ? 'pari' : 'dispari')}.`);
+  }
